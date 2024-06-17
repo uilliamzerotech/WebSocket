e60b5af14fb28c325df765459029931aa0cb931d
Uiiliam <uilliam.fagner@gmail.com>
2024-06-17 08:47:02 -0300

    Adicionar arquivos de projeto.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FuelmanagerWebSocketClient.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string usuario, string mensagem)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", usuario, mensagem);
+        }
+    }
+}
